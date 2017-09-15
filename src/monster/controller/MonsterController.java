86a9b25c57bc2b3d786fe919b010b4ece7fc98cf
greenRose2018 <brifun13@gmail.com>
2017-09-15 09:32:27 -0600

    How to use GET/SET, and using return, and practice.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,10 +1,33 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+import java.util.Scanner;
+
 public class MonsterController
 {
 	public void start()
 	{
+		MarshmallowMonster sample = new MarshmallowMonster();
+		System.out.println(sample);
+		MarshmallowMonster realMonster = new MarshmallowMonster("Fred", 1,2,2,true);
 		
+		System.out.println(realMonster);
+		System.out.println("Brittney is hungry, so he is going to eat a tentacle");
+		realMonster.setTentacleAmount(2);
+		System.out.println(realMonster);
+		
+		interactWithTheMonster(realMonster);
+	}
+	
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 	}
 
 }
