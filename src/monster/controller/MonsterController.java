c70ec5482fdacf8222405c93c455d9ed7d4589a0
greenRose2018 <brifun13@gmail.com>
2017-09-21 08:44:34 -0600

    More practing of popup.displayText, learning to use helper method, and using getResponse

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -8,6 +8,7 @@ public class MonsterController
 {
 	private MonsterDisplay popup;
 	
+	
 	public MonsterController()
 	{
 		popup = new MonsterDisplay();
@@ -30,32 +31,43 @@ public class MonsterController
 	}
 	
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+
 	{
 //		System.out.println(currentMonster.getName() + " wants to know what to eat next");
 		popup.displayText(currentMonster.getName() + " wants to know what to eat next");
 //		System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
-		
+		popup.displayText(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
 //		System.out.println("How many do you want to eat?");
+		
+		int specialAnswer;
+		String unconverted = popup.getResponse("How many do you want to eat?");
+		
+		specialAnswer =  Integer.parseInt(unconverted);
+		
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
 		
 		if(consumed < 0)
 		{
-			System.out.println("You cannot eat a negative amount silly human.");
+			//System.out.println("You cannot eat a negative amount silly human.");
+			popup.displayText("You cannot eat a negative amount silly human.");
 		
 		}
 		else if (consumed == 0)
 		{
-			System.out.println("Not that hungry are you");
+			//System.out.println("Not that hungry are you");
+			popup.displayText("Not that hungry are you");
 		}
 		else if (consumed > currentMonster.getArmCount())
 		{
-			System.out.println("that is impossible - I only have" + currentMonster.getArmCount()+"arms!!!");
+			//System.out.println("that is impossible - I only have" + currentMonster.getArmCount()+"arms!!!");
+			popup.displayText("that is impossible - I only have " + currentMonster.getArmCount()+ "arms!!!");
 		}
 		else
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			System.out.println("Thank you I only havve this many arms now: " + currentMonster.getArmCount());
+			//System.out.println("Thank you I only havve this many arms now: " + currentMonster.getArmCount());
+			popup.displayText("Thank you I only havve this many arms now: " + currentMonster.getArmCount());
 		}
 		
 		System.out.println("How many eyes do you Want to eat - I only have " + currentMonster.getEyeCount());
@@ -94,4 +106,21 @@ public class MonsterController
 		popup.displayText(answer);
 	}
 
+	//Helper methods
+	private boolean isValidInteger(String sample)
+	{
+		boolean valid = false;
+		
+		try
+		{
+			Integer.parseInt(sample);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			popup.displayText("Only integervalues are valid: " + sample + " is not");
+		}
+		
+		return valid;
+	}
 }
