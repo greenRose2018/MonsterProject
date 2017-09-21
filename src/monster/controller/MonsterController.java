76010b184718d70e30c75f26c6ae2602a46183e2
greenRose2018 <brifun13@gmail.com>
2017-09-21 08:08:26 -0600

    Learning to use popup.displayText, and learning getResponse

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -15,22 +15,27 @@ public class MonsterController
 	public void start()
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
-		System.out.println(sample);
+		//System.out.println(sample);
+		popup.displayText(sample.toString());
 		MarshmallowMonster realMonster = new MarshmallowMonster("Fred", 1,2,2,true);
 		
-		System.out.println(realMonster);
-		System.out.println("Brittney is hungry, so he is going to eat a tentacle");
+	//	System.out.println(realMonster);
+		popup.displayText(realMonster.toString());
+	//	System.out.println("Brittney is hungry, so he is going to eat a tentacle");
+		popup.displayText("Brittney is hungry, so she is going to eat a tentacle");
 		realMonster.setTentacleAmount(2);
-		System.out.println(realMonster);
-		
+	//	System.out.println(realMonster);
+		popup.displayText(realMonster.toString());		
 		interactWithTheMonster(realMonster);
 	}
 	
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{
-		System.out.println(currentMonster.getName() + " wants to know what to eat next");
-		System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
-		System.out.println("How many do you want to eat?");
+//		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		popup.displayText(currentMonster.getName() + " wants to know what to eat next");
+//		System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
+		
+//		System.out.println("How many do you want to eat?");
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
 		
